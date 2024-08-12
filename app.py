9a1658dbbba0d7563ad859e7a1d2a7f13f368d4a
Victor Ferreira <husafedj@gmail.com>
2024-08-11 21:55:15 -0300

    Ajuste de caminhos para o render.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re  # Importa o módulo re para expressões regulares
 
 # Define o caminho para o executável do Tesseract, necessário se o caminho não estiver no PATH do sistema
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 # Ajuste o caminho se necessário
 pytesseract.pytesseract.tesseract_cmd = '/path/to/tesseract'
